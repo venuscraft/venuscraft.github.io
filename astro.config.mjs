a9604649cfa3b91ea492d36207a782606c338696
GalactHD <139361517+GalactHD@users.noreply.github.com>
2025-01-01 17:42:59 -0300

    correções

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://venuscraft.github.io',
+  base: '/venuscraft.github.io',
 });
